741e105b61d9635c2d673ec06d992ad7ea087fa4
Kevin Barrett <kevin.barrett@postlight.com>
2021-05-21 09:39:46 -0400

    feat: initial working version

--- main.ts
+++ main.ts
@@ -1,112 +1,104 @@
-import { App, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+  App,
+  Plugin,
+  FuzzyMatch,
+  FuzzySuggestModal,
+  MarkdownView,
+} from "obsidian";
 
-interface MyPluginSettings {
-	mySetting: string;
+// @ts-ignore
+import electron from "electron";
+import TurndownService from "turndown";
+
+export default class Pluck extends Plugin {
+  async onload() {
+    // Intercept Cross-Origin.
+    // H/t https://discord.com/channels/686053708261228577/707816848615407697/802052942885945395
+    // @ts-ignore
+    electron.remote.session.defaultSession.webRequest.onHeadersReceived(
+      { urls: ["https://*/*", "http://*/*"] },
+      (details: any, callback: any) => {
+        console.log("in callback");
+        callback({
+          responseHeaders: {
+            ...details.responseHeaders,
+            "x-frame-options": [""],
+            "access-control-allow-origin": ["*"],
+          },
+        });
+      }
+    );
+
+    this.addStatusBarItem().setText("Status Bar Text");
+
+    this.addCommand({
+      id: "pluck-insert-from-url",
+      name: "Insert contents from URL",
+      callback: () => {
+        new URLModal(this.app, this).open();
+      },
+    });
+  }
+
+  async processURL(url: string) {
+    const response = await fetch(url, {
+      headers: {
+        Accept: "text/plain",
+      },
+    });
+    const res = await response.text();
+    if (!res) {
+      console.error(`[Pluck] No text/plain response from URL: ${url}`);
+      return;
+    }
+    const turndownService = new TurndownService();
+    const md = turndownService.turndown(res);
+    if (!md) {
+      console.error(`[Pluck] Unable to convert text to Markdown.`);
+      return;
+    }
+
+    const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
+    if (!activeView) {
+      console.error("[Pluck] No active view to insert into.");
+      return;
+    }
+
+    const editor = activeView.editor;
+    const doc = editor.getDoc();
+    doc.replaceSelection(md);
+  }
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+class URLModal extends FuzzySuggestModal<string> {
+  plugin: Pluck;
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+  constructor(app: App, plugin: Pluck) {
+    super(app);
+    this.plugin = plugin;
+  }
 
-	async onload() {
-		console.log('loading plugin');
+  getSuggestions(query: string): FuzzyMatch<string>[] {
+    return [
+      {
+        item: query,
+        match: {
+          score: 0,
+          matches: [],
+        },
+      },
+    ];
+  }
 
-		await this.loadSettings();
+  getItems(): string[] {
+    return [];
+  }
 
-		this.addRibbonIcon('dice', 'Sample Plugin', () => {
-			new Notice('This is a notice!');
-		});
+  getItemText(item: string): string {
+    return `Insert HTML -> Markdown from: ${item}`;
+  }
 
-		this.addStatusBarItem().setText('Status Bar Text');
-
-		this.addCommand({
-			id: 'open-sample-modal',
-			name: 'Open Sample Modal',
-			// callback: () => {
-			// 	console.log('Simple Callback');
-			// },
-			checkCallback: (checking: boolean) => {
-				let leaf = this.app.workspace.activeLeaf;
-				if (leaf) {
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-					return true;
-				}
-				return false;
-			}
-		});
-
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		this.registerCodeMirror((cm: CodeMirror.Editor) => {
-			console.log('codemirror', cm);
-		});
-
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-		console.log('unloading plugin');
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		let {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		let {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue('')
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+  onChooseItem(item: string, evt: MouseEvent | KeyboardEvent): void {
+    this.plugin.processURL(item);
+  }
 }
